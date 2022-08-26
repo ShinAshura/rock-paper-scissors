5808d514301856624b498b9e48711f258022cd5b
Shin <luffy-karugara@hotmail.fr>
2022-08-26 19:55:13 +0200

    Create playSingleRound function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let rock = 'rock';
+let paper = 'paper';
+let scissors = 'scissors';
+
 function getComputerChoice() {
-    let rock = 'ŕock';
+    let rock = 'rock';
     let paper = 'paper';
     let scissors = 'scissors';
     let random = Math.ceil(Math.random() * 3);
@@ -11,4 +15,38 @@ function getComputerChoice() {
         return paper;
     }
     return scissors;
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+    // console.log(playerSelection);
+    // console.log(computerSelection);
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+        console.log('It\'s a draw!');
+    }
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        console.log('You lose! Paper beats Rock');
+    }
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+        console.log('You win! Rock beats Scissors');
+    }
+
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        console.log('You win! Paper beats Rock');
+    }
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
+        console.log('It\'s a draw!');
+    }
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+        console.log('You lose! Scissors beats Paper');
+    }
+
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+        console.log('You lose! ROck beats Scissors');
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        console.log('You win! Scissors beats paper');
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
+        console.log('It\'s a draw!');
+    }
 }
