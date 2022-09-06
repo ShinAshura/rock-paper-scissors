258e32749eb719fb140d21f4ea9516c3dd26e220
Shin <luffy-karugara@hotmail.fr>
2022-09-06 16:32:45 +0200

    Revisit rock paper scissors

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let rock = 'rock';
 let paper = 'paper';
 let scissors = 'scissors';
+let playerCount = 0;
+let computerCount = 0;
 const win = 'win';
 const lose = 'lose';
 const draw = 'draw';
+const result = document.querySelector('.result');
 
 function getComputerChoice() {
     let rock = 'rock';
@@ -20,42 +23,146 @@ function getComputerChoice() {
     return scissors;
 }
 
-function playRound(playerSelection, computerSelection) {
+function testWin(score) {
+    if (score === 5) {
+        result.textContent = 'You WIN!';
+        alert('you WIN !');
+    }
+}
+
+function testLose(score) {
+    if (score === 5) {
+        result.textContent = 'You LOSE!';
+        alert('you LOSE !');
+    }
+}
+
+function playRound(playerSelection) {
     // const win = 'win';
     // const lose = 'lose';
     // const draw = 'draw';
     // console.log(playerSelection);
     // console.log(computerSelection);
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
-        return draw;
-    }
-    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        return lose;
-    }
-    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        return win;
+    // let playerCount = 0;
+    // let computerCount = 0;
+    computerSelection = getComputerChoice();
+    // const result = document.querySelector('.result');
+    const counter = document.querySelector('.counter');
+    // const key = document.querySelector("playerSelection");
+    // const btn = $(this).val();
+    // console.log(result);
+
+    if (playerCount === 5 || computerCount === 5) {
+        return;
     }
 
-    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        return win;
+    if (this.value === 'rock' && computerSelection === 'rock') {
+        // return draw;
+        result.textContent = 'draw';
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('draw');
     }
-    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
-        return draw;
+    else if (this.value === 'rock' && computerSelection === 'paper') {
+        // return lose;
+        result.textContent = 'lose';
+        computerCount += 1;
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('lose');
+        testLose(computerCount);
     }
-    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-        return lose;
+    else if (this.value === 'rock' && computerSelection === 'scissors') {
+        // return win;
+        result.textContent = 'win';
+        playerCount += 1;
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('win');
+        testWin(playerCount);
     }
 
-    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-        return lose;
+    else if (this.value === 'paper' && computerSelection === 'rock') {
+        // return win;
+        result.textContent = 'win';
+        playerCount += 1;
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('win');
+        testWin(playerCount);
     }
-    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        return win;
+    else if (this.value === 'paper' && computerSelection === 'paper') {
+        // return draw;
+        result.textContent = 'draw';
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('draw');
     }
-    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
-        return draw;
+    else if (this.value === 'paper' && computerSelection === 'scissors') {
+        // return lose;
+        result.textContent = 'lose';
+        computerCount += 1;
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('lose');
+        testLose(computerCount);
+    }
+
+    else if (this.value === 'scissors' && computerSelection === 'rock') {
+        // return lose;
+        result.textContent = 'lose';
+        computerCount += 1;
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('lose');
+        testLose(computerCount);
+    }
+    else if (this.value === 'scissors' && computerSelection === 'paper') {
+        // return win;
+        result.textContent = 'win';
+        playerCount += 1;
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('win');
+        testWin(playerCount);
+    }
+    else if (this.value === 'scissors' && computerSelection === 'scissors') {
+        // return draw;
+        result.textContent = 'draw';
+        counter.textContent = playerCount + ' - ' + computerCount;
+        console.log('draw');
     }
 }
+
+
+// function playRound(playerSelection, computerSelection) {
+//     // const win = 'win';
+//     // const lose = 'lose';
+//     // const draw = 'draw';
+//     // console.log(playerSelection);
+//     // console.log(computerSelection);
+//     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+//         return draw;
+//     }
+//     else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+//         return lose;
+//     }
+//     else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+//         return win;
+//     }
+
+//     else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+//         return win;
+//     }
+//     else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
+//         return draw;
+//     }
+//     else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+//         return lose;
+//     }
+
+//     else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+//         return lose;
+//     }
+//     else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+//         return win;
+//     }
+//     else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
+//         return draw;
+//     }
+// }
 
 function game() {
     let playerCount = 0;
@@ -114,7 +221,18 @@ function game() {
     }
 }
 
-game();
+// game();
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound)
+        // alert(button.id);
+});
+// });
+
+
+
 
 // console.log(playRound(paper, getComputerChoice()));
 
@@ -156,3 +274,56 @@ game();
 //         return 'It\'s a draw!';
 //     }
 // }
+
+
+
+
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', function playRound(playerSelection) {
+//         computerSelection = getComputerChoice();
+//         const key = document.getElementById("rps");
+//         // const btn = $(this).val();
+//         console.log(key.value);
+//         if (playerSelection === 'rock' && computerSelection === 'rock') {
+//             // return draw;
+//             console.log('draw');
+//         }
+//         else if (playerSelection === 'rock' && computerSelection === 'paper') {
+//             // return lose;
+//             console.log('lose');
+//         }
+//         else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+//             // return win;
+//             console.log('win');
+//         }
+
+//         else if (playerSelection === 'paper' && computerSelection === 'rock') {
+//             // return win;
+//             console.log('win');
+//         }
+//         else if (playerSelection === 'paper' && computerSelection === 'paper') {
+//             // return draw;
+//             console.lof('draw');
+//         }
+//         else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+//             // return lose;
+//             console.log('lose');
+//         }
+
+//         else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+//             // return lose;
+//             console.log('lose');
+//         }
+//         else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+//             // return win;
+//             console.log('win');
+//         }
+//         else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+//             // return draw;
+//             console.lof('draw');
+//         }
+
+//     });
+//     // alert(button.id);
+// });
